e60807f2f1a4441c34d92c73f8475c096b1cd23c
Dan Rosen <dan@marakana.com>
2013-01-24 14:36:12 -0800

    created worker...

--- client-server/src/com/marakana/clientserver/Server.java
+++ client-server/src/com/marakana/clientserver/Server.java
@@ -8,15 +8,26 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.marakana.concurrency.Actor;
+
 public class Server {
 
-	public static void main(String[] args) {
-		ExecutorService pool = Executors.newCachedThreadPool();
+	public static enum Message {
+		SHUTDOWN
+	}
 
-		try {
-			ServerSocket server = new ServerSocket(31337);
+	public static class Worker extends Actor {
+		@Override
+		protected void receive(Object message) {
+			if (message == Message.SHUTDOWN) {
+				shutdown();
+			} else if (message instanceof Socket) {
+				work((Socket) message);
+			}
+		}
+
+		private void work(Socket client) {
 			try {
-				Socket client = server.accept();
 				try {
 					BufferedReader in = new BufferedReader(
 							new InputStreamReader(client.getInputStream()));
@@ -28,6 +39,20 @@ public class Server {
 				} finally {
 					client.close();
 				}
+			} catch (IOException e) {
+				// TODO: do something with this exception
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ExecutorService pool = Executors.newCachedThreadPool();
+
+		try {
+			ServerSocket server = new ServerSocket(31337);
+			try {
+				Socket client = server.accept();
+				// TODO: make an actor that uses the client connection
 			} finally {
 				server.close();
 			}
