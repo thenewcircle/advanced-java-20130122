0615939e54c26495f2b79dfe6023636853a5b002
Dan Rosen <dan@marakana.com>
2013-01-24 12:02:37 -0800

    proper exception handling

--- client-server/src/com/marakana/clientserver/Client.java
+++ client-server/src/com/marakana/clientserver/Client.java
@@ -1,16 +1,28 @@
 package com.marakana.clientserver;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception {
-		Socket server = new Socket(InetAddress.getLocalHost(), 31337);
-		PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-		out.println("Hello, Server!");
-		server.close();
+	public static void main(String[] args) {
+		try {
+			Socket server = new Socket(InetAddress.getLocalHost(), 31337);
+			try {
+				PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+				try {
+					out.println("Hello, Server!");
+				} finally {
+					out.close();
+				}
+			} finally {
+				server.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- client-server/src/com/marakana/clientserver/Server.java
+++ client-server/src/com/marakana/clientserver/Server.java
@@ -1,18 +1,35 @@
 package com.marakana.clientserver;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-	public static void main(String[] args) throws Exception {
-		ServerSocket server = new ServerSocket(31337);
-		Socket client = server.accept();
-		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		System.out.println(in.readLine());
-		server.close();
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(31337);
+			try {
+				Socket client = server.accept();
+				try {
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(client.getInputStream()));
+					try {
+						System.out.println(in.readLine());
+					} finally {
+						in.close();
+					}
+				} finally {
+					client.close();
+				}
+			} finally {
+				server.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
